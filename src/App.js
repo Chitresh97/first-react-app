e9bf46af2ad89ad3c624649cdffffe1a0d7f4e81
Chitresh <chitu1997@gmail.com>
2019-01-30 08:02:58 +0530

    person component created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-
 import './App.css';
+// importing person component//
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
         <h1>Hello</h1>
-      
+        {/* Using the person component, self closing tag */}
+        <Person/>
       </div>
     );
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+//No need to import component because we are not creating class that inherits component class.
+
+//Component is basically a function returning JSX
+const person=()=>{
+    return <p>I'm a person.</p>
+}
+
+export default person;
